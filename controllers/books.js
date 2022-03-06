002a95488518aaab4b1b1ebe2318a5ddc452c3ab
jgl3hb <lewis.jeffreyg@gmail.com>
2022-03-05 21:47:46 -0500

    mvc

---
+++ controllers/books.js
@@ -0,0 +1,20 @@
+import { Book } from '../models/book.js'
+
+function index(req, res) {
+  Book.find({})
+  .then(books => {
+    res.render('books/index', {
+      books,
+      title: "🌮"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/tacos")
+  })
+}
+
+export {
+  index
+}
+
---
+++ models/book.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const bookSchema = new Schema({
+  name: String,
+  author: String,
+  category: {
+    type: String,
+    enum: ['Sci-Fi', 'Action', 'Fantasy','Suspense/Thriller','Western', 'Computer Science', 'Philosophy/Psych', 'Social Sciences', 'Technology', 'History/Geography'],
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+  },
+  read: Boolean,
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+export {
+  Book
+}
