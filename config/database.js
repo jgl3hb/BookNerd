ce13f67f13239c24bd21aeecd73338e1c5720b20
jgl3hb <lewis.jeffreyg@gmail.com>
2023-10-18 00:10:15 -0400

    mongodb reset

--- config/database.js
+++ config/database.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const dbName = process.env.MONGODB_DB;
+const dbURI = 'mongodb://localhost:27017/booknerd'  
 
 const db = mongoose.connection
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.set('strictQuery', true)
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // database connection event
 db.on('connected', function () {
